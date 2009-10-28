8fd219c90d8bf2c5ea1fb61edd293cebbeea53cd
miguelcpjava <miguelcpjava@3540a1b2-a9c5-11de-be8d-735280eae207>
2009-10-28 12:12:30 +0000

    Adicionando a classe cliente

---
+++ src/Cadastro/Cliente.java
@@ -0,0 +1,18 @@
+package Cadastro;
+
+public class Cliente {
+
+	
+	String Titulo;
+	String Telefone;
+	String CPF;
+	String Endereço;
+	
+	
+	public Cliente(String titulo,String telefone,String CPF,String endereço ){
+		this.Titulo = titulo;
+		this.Telefone = telefone;
+		this.CPF = CPF;
+		this.Endereço = endereço;
+	}
+}
